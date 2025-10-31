1b56bc55f3e9c25961cdca9025f1a6b3d4cb8bb6
Haha64142 <paul.w64142@gmail.com>
2025-10-31 01:47:16 -0500

    Add a way to stop the server
    If you press the Enter key in the terminal, it will stop the program

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import path from "path";
+import readline from "readline";
 import { fileURLToPath } from "url";
 import { getResponse } from "./ai.js";
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,12 +16,25 @@ app.post("/api/data", async (req, res) => {
   const data = req.body;
   console.log("Received data:", data);
   const response = await getResponse(data);
-  console.log("Sending response:", response);
   res.json(response);
 });
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+const server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  console.log("\nPress Enter to stop the server");
+});
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", () => {
+  console.log("Stopping server...");
+  server.close(() => {
+    console.log("Server stopped");
+    process.exit(0);
+  });
 });
